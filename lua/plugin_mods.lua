f2eac6425a13d2671a9c9eb7492809cdd87dd053
IsaacWSL <isaacmorrowofficial@gmail.com>
2024-07-06 16:33:56 -0700

    initial commit

---
+++ lua/plugin_mods.lua
@@ -0,0 +1,44 @@
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = { "node_modules" },  -- Ignore node_modules directory
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = false, -- when true, they will just be displayed differently than normal items
+      hide_dotfiles = false,
+      hide_gitignored = true,
+      hide_hidden = true, -- only works on Windows for hidden files/directories
+      hide_by_name = {
+        ".DS_Store",
+        "thumbs.db",
+        --"node_modules",
+      },
+      hide_by_pattern = {
+        --"*.meta",
+        --"*/src/*/tsconfig.json",
+      },
+      always_show = { -- remains visible even if other settings would normally hide it
+        --".gitignored",
+      },
+      never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+        --".DS_Store",
+        --"thumbs.db",
+      },
+      never_show_by_pattern = { -- uses glob style patterns
+        --".null-ls_*",
+      },
+    },
+  }
+})
