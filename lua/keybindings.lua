10313eb55f4c321cdd82fa34f1e56f1684488065
IsaacArch <isaacmorrowofficial@gmail.com>
2025-01-07 20:13:22 -0800

    Added grep & term keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -6,7 +6,9 @@ vim.g.copilot_no_tab_map = true
 
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
+-- Custom keybindings with , as leader key
 vim.api.nvim_set_keymap('n', ',g', ':Telescope live_grep<CR>', { noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', ',t', ':ToggleTerm direction=float<CR>', { noremap = true, silent = false})
 
 
 
